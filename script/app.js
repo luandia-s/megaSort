29db116bf846c3d4aef25504403f51237a93c4ad
Luan Dias <luandias1312@gmail.com>
2020-12-17 23:36:06 -0300

    adição de método para remoção de números repetidos

--- script/app.js
+++ script/app.js
@@ -1,10 +1,86 @@
 let numberDrawn = []
 let results = document.getElementById('results')
+let btn = document.getElementById('enviar')
+let select = document.getElementById('games')
+let inputNumbers = document.getElementById('numbers-played')
+
+select.addEventListener('change', (event) => {
+    console.log(event.target.value)
+    switch (event.target.value) {
+        case 'mega-sena':
+            inputNumbers.value = '6'
+            inputNumbers.setAttribute('min', '6')
+            inputNumbers.setAttribute('max', '15')
+            console.log(inputNumbers)
+            break;
+        case 'quina':
+            inputNumbers.value = '5'
+            inputNumbers.setAttribute('min', '5')
+            inputNumbers.setAttribute('max', '15')
+
+            break;
+        case 'lotofácil':
+            inputNumbers.value = '15'
+            inputNumbers.setAttribute('min', '15')
+            inputNumbers.setAttribute('max', '18')
+            break;
+        case 'lotomania':
+            inputNumbers.value = '50'
+            inputNumbers.setAttribute('min', '50')
+            inputNumbers.setAttribute('max', '50')
+            break;
+        default:
+            break;
+    }
+})
+
+btn.addEventListener('click', generateNumbers)
 
 function generateNumbers() {
 
-    for (let i=0; i<6; i++) {
-        numberDrawn.push(Math.floor(Math.random() * 60))
+    let input = document.getElementById('numbers-played')
+    let max = input.getAttribute('max')
+    let min = input.getAttribute('min')
+
+    if (input.value >= min || input.value <= max) {
+        switch (select.value) {
+            case 'mega-sena':
+                while (numberDrawn.length < input.value) {
+                    let numeroSorteado = Math.floor(Math.random() * 60) + 1
+                    if (!numberDrawn.includes(numeroSorteado)) {
+                        numberDrawn.push(numeroSorteado)
+                    }
+                }
+                break;
+            case 'quina':
+                while (numberDrawn.length < inputNumbers.value) {
+                    let numeroSorteado = Math.floor(Math.random() * 80) + 1
+                    if (!numberDrawn.includes(numeroSorteado)) {
+                        numberDrawn.push(numeroSorteado)
+                    }
+                }
+                break;
+            case 'lotofácil':
+                while (numberDrawn.length < inputNumbers.value) {
+                    let numeroSorteado = Math.floor(Math.random() * 25) + 1
+                    if (!numberDrawn.includes(numeroSorteado)) {
+                        numberDrawn.push(numeroSorteado)
+                    }
+                }
+                break;
+            case 'lotomania':
+                while (numberDrawn.length < inputNumbers.value) {
+                    let numeroSorteado = Math.floor(Math.random() * 99)
+                    if (!numberDrawn.includes(numeroSorteado)) {
+                        numberDrawn.push(numeroSorteado)
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    } else {
+        window.alert('preencha os campos corretamente!')
     }
 
     numberDrawn.sort((a, b) => a - b)
@@ -15,13 +91,12 @@ function generateNumbers() {
 function updateScren() {
     let results = document.getElementById('results')
     results.innerHTML = ""
-    numberDrawn.forEach( item => {
+    numberDrawn.forEach(item => {
         let divNumber = document.createElement('div')
         let textDivNumber = document.createTextNode(item.toString())
         divNumber.setAttribute('class', 'number')
         divNumber.appendChild(textDivNumber)
         results.appendChild(divNumber)
-        console.log(divNumber)
     })
     numberDrawn = []
 }
